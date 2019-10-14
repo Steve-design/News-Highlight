be04300eabaf98dedf80f6fb5fc7b4bf3fd0f30c
Steve-design <smzalendo31@gmail.com>
2019-10-14 12:48:38 +0300

    create a subclass  for NewsTest

--- tests/tests_news.py
+++ tests/tests_news.py
@@ -1,2 +1,9 @@
 import unittest
 from app.models import News,Articles
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+    
