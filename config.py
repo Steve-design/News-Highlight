7605068f5e6de3c04cf68bd906ce96226683633a
Steve-design <smzalendo31@gmail.com>
2019-10-14 10:56:12 +0300

    create a configuration class for general configuration

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    NEWS_API_BASE_URL ="https://newsapi.org/v2/sources?language=en&category={}&apiKey={}"
+    ARTICLES_BASE_URL = "https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}"
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    @staticmethod
+    def init_app(app):
+        pass
