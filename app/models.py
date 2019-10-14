29ce9768754d76922b933e14be1f2c19f2cfc008
Steve-design <smzalendo31@gmail.com>
2019-10-14 11:31:28 +0300

    create an article class to define artcle objects

--- app/models.py
+++ app/models.py
@@ -12,3 +12,17 @@ class News:
         self.category = category
         self.country = country
         self.language = language
+
+class Articles:
+     '''
+     Articles class to define articles objects
+     '''
+     def __init__(self,id,name,title,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.name = name
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
