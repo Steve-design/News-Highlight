4916fdbebbbc1b1e3bad0c2c9e64dd46ad0b2152
Steve-design <smzalendo31@gmail.com>
2019-10-14 14:47:39 +0300

    register the blueprint and set configuration

--- app/_init_.py
+++ app/_init_.py
@@ -12,3 +12,13 @@ def create_app(config_name):
 
 	# Initializing flask extensions
 	bootstrap.init_app(app)
+
+    #registering the blueprint
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	#setting config
+	from .requests import configure_request
+	configure_request(app)
+
+	return app
