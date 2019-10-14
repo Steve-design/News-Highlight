0581acd51c5a5a7012deb300557c08d13464ab30
Steve-design <smzalendo31@gmail.com>
2019-10-14 14:48:26 +0300

    complete the initilization

--- app/_init_.py
+++ app/_init_.py
@@ -17,7 +17,7 @@ def create_app(config_name):
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
 
-	#setting config
+	#setting configuration
 	from .requests import configure_request
 	configure_request(app)
 
