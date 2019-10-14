c2554dc15bb42a9020bb2ab512bb08a79af9dae1
Steve-design <smzalendo31@gmail.com>
2019-10-14 12:31:52 +0300

    add a function that views articles page

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,14 @@ def index():
     business_news = get_news("business")
     title = "News App - Where news live on"
     return render_template('index.html' , title = title, sports_news = sports_news,entertainment_news = entertainment_news, business_news = business_news)  #, 
+
+@main.route('/news/<id>')
+def articles(id):
+	'''
+	view articles page
+	'''
+	articles = get_articles(id)
+
+	title = f'NH | {id}'
+
+	return render_template('articles.html',title= title,articles = articles)
