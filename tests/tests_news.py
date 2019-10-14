e99ee4b22499adaa7e190f8d530e96615bf612a4
Steve-design <smzalendo31@gmail.com>
2019-10-14 12:50:06 +0300

    add a setup method to run before every test

--- tests/tests_news.py
+++ tests/tests_news.py
@@ -4,6 +4,10 @@ News = news.News
 
 class NewsTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the Movie class
+    Test Class to test the behaviour of the News class
     '''
-    
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''"
+        self.new_news = News(id,"name","description","url","category","country","language")
