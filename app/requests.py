0c4d0cbe4cc210bab9f4b882c51ba3b495e7ed6b
Steve-design <smzalendo31@gmail.com>
2019-10-14 11:56:11 +0300

    add a function to nprocess the news result

--- app/requests.py
+++ app/requests.py
@@ -34,3 +34,26 @@ def get_news(category):
             news_results = process_results(news_results_list)
 
     return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+    Args:
+        news_list: A list of dictionaries that contain news details
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id') 
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        language = news_item.get('language')
+        country = news_item.get('country')
+        
+        news_object = News(id,name,description,url,category,country,language)
+        news_results.append(news_object)
+
+    return news_results    
