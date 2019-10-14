fce633e247505f46d31914022b9e8fe3c56a0d3a
Steve-design <smzalendo31@gmail.com>
2019-10-14 11:01:22 +0300

    create a production configuration child class

--- config.py
+++ config.py
@@ -11,3 +11,11 @@ class Config:
     @staticmethod
     def init_app(app):
         pass
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass    
