5d317eb6b632bf708045169f4af96a2c5a38e58a
Steve-design <smzalendo31@gmail.com>
2019-10-14 11:48:08 +0300

    define functions that link the urls

--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,15 @@
 import urllib.request,json
 from models import News, Articles
+
+# Getting api key
+apiKey = None
+# Getting the news base url
+base_url = None
+# Getting the articles base url
+articles_url = None
+
+def configure_request(app):
+	global apiKey,base_url,articles_url
+	apiKey = app.config["NEWS_API_KEY"]
+	base_url = app.config["NEWS_API_BASE_URL"]
+	articles_url = app.config["ARTICLES_BASE_URL"]
